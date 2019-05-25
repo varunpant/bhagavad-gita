4e7c559c4c4add8c858ab9d8e4b0c34fdac83d2f
varun <varun@varunpant.com>
2019-05-25 20:36:04 +0100

    rebuilding site Sat 25 May 2019 20:36:04 BST

--- docs/js/main.js
+++ docs/js/main.js
@@ -41,6 +41,77 @@ for (i = 0; i < acc.length; i++) {
 }
 
 }
+
+
+var getParams = function(navlink){
+  var l = document.createElement("a");
+  l.href = navlink;
+  try{
+    var chapter = l.pathname.match(/chapter-(\d+)*/,"")[1];
+    var sutra = l.pathname.match(/sutra-(\d+)*/,"")[1];
+    return {
+      "c":chapter,
+      "s":sutra,
+      "h":l.hostname
+    }
+  }
+  catch{
+    return null,null,l.hostname;
+  }
+ 
+}
+
+var preopenAccordion = function(chapter){
+  document.getElementById("btn"+chapter).click();
+}
+var initAppState = function(){
+  var KEY = "VERSE"
+  if (localStorage){
+
+    window.onbeforeunload = function () {
+      var navlink = document.activeElement.href;
+      if(navlink){
+        var params = getParams(navlink);
+        var chapter = params.c;
+        var sutra = params.s;
+        var hostname = params.h;
+       
+        if(hostname === window.location.hostname){
+           if(!isNaN(chapter) && !isNaN(sutra)){
+            var verse = chapter + "," + sutra;
+            localStorage[KEY] = verse;
+            console.log("state saved"+verse)
+           }
+        }
+      } 
+  };
+
+
+    var state = localStorage[KEY] ;
+    if(state){
+              var verse = state.split(",");
+              var chapter = verse[0];
+              var sutra = verse[1];
+              var params = getParams(window.location.href);
+              var _chapter = params.c;
+              var _sutra = params.s;
+              preopenAccordion(_chapter);
+              if(!_chapter && !_sutra){
+                var url = location.protocol + "//" + window.location.host + "/chapter-" + chapter + "/sutra-" +sutra;
+                location.assign(url);
+              }
+             
+    }else{
+      console.log("previous state not found.")
+    }
+  }
+  else{
+    console.log("Localstorage not supported.")
+  }
+ 
+}
+
 onload = function(){
   bindAccordion();
+  initAppState();
 }
